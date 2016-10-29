db3cb392a7eac9c5606734d01224b6797902fdca
Jeremy.Hutchinson <cower3.1@gmail.com>
2016-10-28 20:15:44 -0400

    I'm a dumb

--- cogs/MuvLuv.py
+++ cogs/MuvLuv.py
@@ -9,19 +9,19 @@ class MuvLuv:
 
     async def on_member_join(self, member):
         if member.server.id == self.ml_server:
-            await self.bot.send_message(self.ml_announce, "@here Member joined: {a.name}, {a.mention}".format(member))
+            await self.bot.send_message(self.ml_announce, "@here Member joined: {a.name}, {a.mention}".format(a=member))
 
     async def on_member_remove(self, member):
         if member.server.id == self.ml_server:
-            await self.bot.send_message(self.ml_announce, "Member left: {a.name}, ({a.id})".format(member))
+            await self.bot.send_message(self.ml_announce, "Member left: {a.name}, ({a.id})".format(a=member))
 
     async def on_member_ban(self, member):
         if member.server.id == self.ml_server:
-            await self.bot.send_message(self.ml_announce, "🔨🔨 Member banned: {a.name}, ({a.id})".format(member))
+            await self.bot.send_message(self.ml_announce, "🔨🔨 Member banned: {a.name}, ({a.id})".format(a=member))
 
     async def on_member_unban(self, member):
         if member.server.id == self.ml_server:
-            await self.bot.send_message(self.ml_announce, "Member unbanned: {a.name}, ({a.id})".format(member))
+            await self.bot.send_message(self.ml_announce, "Member unbanned: {a.name}, ({a.id})".format(a=member))
 
 
 def setup(bot):
