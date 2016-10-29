113898e176a2cfce6d7c14bef87b68f856541f05
Jeremy.Hutchinson <cower3.1@gmail.com>
2016-10-29 11:40:19 -0400

    I'm a dumb x4

--- cogs/MuvLuv.py
+++ cogs/MuvLuv.py
@@ -8,20 +8,24 @@ class MuvLuv:
         self.ml_announce = "235502028930023424"
 
     async def on_member_join(self, member):
+        announce = await self.bot.get_channel(self.ml_announce)
         if member.server.id == self.ml_server:
-            await self.bot.send_message(await self.bot.get_channel(self.ml_announce), "@here Member joined: {a.name}, {a.mention}".format(a=member))
+            await self.bot.send_message(announce, "@here Member joined: {a.name}, {a.mention}".format(a=member))
 
     async def on_member_remove(self, member):
+        announce = await self.bot.get_channel(self.ml_announce)
         if member.server.id == self.ml_server:
-            await self.bot.send_message(await self.bot.get_channel(self.ml_announce), "Member left: {a.name}, ({a.id})".format(a=member))
+            await self.bot.send_message(announce, "Member left: {a.name}, ({a.id})".format(a=member))
 
     async def on_member_ban(self, member):
+        announce = await self.bot.get_channel(self.ml_announce)
         if member.server.id == self.ml_server:
-            await self.bot.send_message(await self.bot.get_channel(self.ml_announce), "🔨🔨 Member banned: {a.name}, ({a.id})".format(a=member))
+            await self.bot.send_message(announce, "🔨🔨 Member banned: {a.name}, ({a.id})".format(a=member))
 
     async def on_member_unban(self, member):
+        announce = await self.bot.get_channel(self.ml_announce)
         if member.server.id == self.ml_server:
-            await self.bot.send_message(await self.bot.get_channel(self.ml_announce), "Member unbanned: {a.name}, ({a.id})".format(a=member))
+            await self.bot.send_message(announce, "Member unbanned: {a.name}, ({a.id})".format(a=member))
 
 
 def setup(bot):
