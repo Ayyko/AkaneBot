604ef22c2acc9258fa438dbe2292fad0d1091884
Jeremy.Hutchinson <cower3.1@gmail.com>
2016-10-28 20:11:55 -0400

    Catching 0 results

--- cogs/search.py
+++ cogs/search.py
@@ -29,14 +29,18 @@ class Search:
         async with aiohttp.ClientSession() as session:
             async with session.get(self.base_url + "?key={}&cx={}&q={}".format(self.params['key'], self.params['cx'], self.params['q'])) as r:  # built in aiohtttp params thing didn't work so we got this cancer
                 results = await r.json()
-        ret = ""
-        for i in range(num):
-            emoji = await self.emoji_get(i)
-            ret += "{} `{}` <{}>\n".format(emoji, results["items"][i]["title"], results["items"][i]["link"])
-        if num_error:
-            ret += "Results have been limited to 10 because that's how many google returns"
-        ret = "Results for `" + query.replace("@here", "@​here").replace("@everyone", "@​everyone") + "`\n" + ret.replace("@here", "@​here").replace("@everyone", "@​everyone")
-        await self.bot.say(ret[:1999])
+
+        if results["searchInformation"]["totalResults"] is not "0":
+            ret = ""
+            for i in range(num):
+                emoji = await self.emoji_get(i)
+                ret += "{} `{}` <{}>\n".format(emoji, results["items"][i]["title"], results["items"][i]["link"])
+            if num_error:
+                ret += "Results have been limited to 10 because that's how many google returns"
+            ret = "Results for `" + query.replace("@here", "@​here").replace("@everyone", "@​everyone") + "`\n" + ret.replace("@here", "@​here").replace("@everyone", "@​everyone")
+            await self.bot.say(ret[:1999])
+        else:
+            await self.bot.say("No results found for {}".format(query.replace("@here", "@​here").replace("@everyone", "@​everyone")))
         # print(json.dumps(results))
 
     @commands.command(pass_context=True)
@@ -60,16 +64,20 @@ class Search:
         async with aiohttp.ClientSession() as session:
             async with session.get(self.base_url + "?key={}&cx={}&q={}".format(self.params['key'], self.params['cx'], self.params['q'])) as r:  # built in aiohtttp params thing didn't work so we got this cancer
                 results = await r.json()
-        ret = ""
-        for i in range(num):
-            emoji = await self.emoji_get(i)
-            ret += "{} `{}`\n{}\n{}\n\n".format(emoji, results["items"][i]["title"], results["items"][i]["link"], results["items"][i]["snippet"])
-        if num_error == 1:
-            ret += "Results have been limited to {} because results are massive".format(num)
-        if num_error == 2:
-            ret += "Results have been limited to 10 because google only gives me 10 at a time"
-        ret = "Results for `" + query.replace("@here", "@​here").replace("@everyone", "@​everyone") + "`\n" + ret.replace("@here", "@​here").replace("@everyone", "@​everyone")
-        await self.bot.say(ret[:1999])
+
+        if results["searchInformation"]["totalResults"] is not "0":
+            ret = ""
+            for i in range(num):
+                emoji = await self.emoji_get(i)
+                ret += "{} `{}`\n{}\n{}\n\n".format(emoji, results["items"][i]["title"], results["items"][i]["link"], results["items"][i]["snippet"])
+            if num_error == 1:
+                ret += "Results have been limited to {} because results are massive".format(num)
+            if num_error == 2:
+                ret += "Results have been limited to 10 because google only gives me 10 at a time"
+            ret = "Results for `" + query.replace("@here", "@​here").replace("@everyone", "@​everyone") + "`\n" + ret.replace("@here", "@​here").replace("@everyone", "@​everyone")
+            await self.bot.say(ret[:1999])
+        else:
+            await self.bot.say("No results found for {}".format(query.replace("@here", "@​here").replace("@everyone", "@​everyone")))
 
     async def emoji_get(self, i):
         emoji_list = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:', ':keycap_ten:']
