eacfe802861fd93f0fa5992d980921b51d956bf5
Jeremy.Hutchinson <cower3.1@gmail.com>
2017-02-25 14:18:25 -0500

    Added a couple twitter commands

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ It can probably do said stuff, potentially even things.
 Check out the source at https://github.com/Ayyko/AkaneBot
 Made by Ako using discord.py version ''' + discord.__version__
 
-startup_extensions = ["cogs.MuvLuv", "cogs.owner", "cogs.search", "cogs.repl", "cogs.utility"]
+startup_extensions = ["cogs.MuvLuv", "cogs.owner", "cogs.search", "cogs.repl", "cogs.utility", "cogs.twitter"]
 
 async def get_pre(self, message):
     ret = [commands.when_mentioned(self, message), "Akane ", "akane ", "(•ω•) "]
